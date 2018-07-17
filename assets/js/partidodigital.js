568cdb7c308710be62e8d568f8b74d766a9ca8c5
Leo Giovanetti <hello@leog.me>
2018-07-17 20:38:25 -0300

    Update partidodigital.js

--- assets/js/partidodigital.js
+++ assets/js/partidodigital.js
@@ -88,7 +88,7 @@ $(function() {
       $anchor.data("toggle") !== "collapse" &&
       !$anchor.hasClass("close")
     ) {
-      ga(trackerSend, "event", "Link", "Click", $anchor.attr("href").slice(1));
+      ga("send", "event", "Link", "Click", $anchor.attr("href").slice(1));
       $("html, body")
         .stop()
         .animate(
@@ -109,7 +109,7 @@ $(function() {
   // Detectar qué respuesta se visualiza
   $("#accordion").on("show.bs.collapse", function(e) {
     ga(
-      trackerSend,
+      "send",
       "event",
       "Preguntas",
       "VerRespuesta",
@@ -187,7 +187,7 @@ $(function() {
       },
       error: function() {
         if (fbq !== null) fbq("track", "CompleteRegistration");
-        ga(trackerSend, "event", "Formulario", "Enviado", "Firmas");
+        ga("send", "event", "Formulario", "Enviado", "Firmas");
         window.location.href =
           "/hoja?" +
           $.param({
