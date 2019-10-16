68748a7d9ba815bb014150fd8026d2a843dfaee8
Leo Giovanetti <hello@leog.me>
2019-10-16 18:55:27 -0300

    Fix iframe duplication

--- assets/js/propuestas.js
+++ assets/js/propuestas.js
@@ -1,6 +1,8 @@
 $(function () {
     $('.accordion .collapse').on('show.bs.collapse', function (e) {
         var target =  $(e.target);
-        target.find('.iframe-wrapper').append($('<iframe />').attr({src: target.data('url')}))
+        if(target.find('.iframe-wrapper iframe').length === 0) {
+            target.find('.iframe-wrapper').append($('<iframe />').attr({src: target.data('url')}))
+        }
     });
 });
