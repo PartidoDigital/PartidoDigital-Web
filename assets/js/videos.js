314d13bdccae0d997859b5c548f566830963e774
Leo Giovanetti <hello@leog.me>
2019-10-17 21:49:00 -0300

    Carousel de videos usando Slick.js (#26)
    * WIP
    
    * Tweaks

--- assets/js/videos.js
+++ assets/js/videos.js
@@ -1,11 +1,12 @@
+---
+---
+
 $(function() {
     /*
         Carousel
-    */
+        CC 2.0 License Iatek LLC 2018 - Attribution required
+    
     $('#videos-carousel').on('slide.bs.carousel', function (e) {
-        /*
-            CC 2.0 License Iatek LLC 2018 - Attribution required
-        */
         var $e = $(e.relatedTarget);
         var idx = $e.index();
         var itemsPerSlide = 5;
@@ -23,7 +24,39 @@ $(function() {
                 }
             }
         }
-    });
+    });*/
+
+    $('.video-carousel').slick({
+        arrows: true,
+        centerMode: true,
+        centerPadding: '60px',
+        prevArrow: '<a class="carousel-control carousel-control-prev ml-2" href="#videos-carousel" role="button" data-slide="prev">{% include recursos/assets/img/arrow.svg %}<span class="sr-only">Anterior</span></a>"',
+        nextArrow: '<a class="carousel-control carousel-control-next mr-2" href="#videos-carousel" role="button" data-slide="next">{% include recursos/assets/img/arrow.svg %}<span class="sr-only">Siguiente</span></a>',
+        autoplay: true,
+        autoplaySpeed: 2000,
+        slidesToShow: 3,
+        lazyLoad: 'ondemand',
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 3
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 1
+            }
+          }
+        ]
+      });
 
     $('.video-btn').click(function() {
         $videoSrc = "https://www.youtube.com/embed/" + $(this).data("vid");
